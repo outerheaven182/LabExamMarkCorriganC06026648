9a45db2263f413df42b077f065196be131aced78
outerheaven182 <mark.corrigan@yahoo.co.uk>
2014-05-19 18:23:25 +0100

    2nd Commit
    Cloned Bryans Repo and Imported Skyboxes and Particles

---
+++ LabExam/GamesAIBasics-master/Assets/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject leader = GameObject.FindGameObjectWithTag("leader");
+        GameObject teaser = GameObject.FindGameObjectWithTag("teaser");
+
+        leader.GetComponent<StateMachine>().SwitchState(new IdleState(leader, teaser));
+        teaser.GetComponent<StateMachine>().SwitchState(new TeaseState(teaser, leader));
+
+        leader.renderer.material.color = Color.red;
+        teaser.renderer.material.color = Color.blue;
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ LabExam/GamesAIBasics-master/Assets/Path.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Path
+{
+    public List<Vector3> Waypoints = new List<Vector3>();
+    public int next = 0;
+    public bool draw;
+
+    public bool Looped;
+
+    public void Draw()
+    {
+        if (draw)
+        {
+            for (int i = 1; i < Waypoints.Count; i++)
+            {
+                Debug.DrawLine(Waypoints[i - 1], Waypoints[i], Color.cyan);
+            }
+            if (Looped && (Waypoints.Count > 0))
+            {
+                Debug.DrawLine(Waypoints[0], Waypoints[Waypoints.Count - 1], Color.cyan);
+            }
+        }
+    }
+
+    public Vector3 NextWaypoint()
+    {
+        return Waypoints[next];
+    }
+
+    public bool IsLast()
+    {
+        return (next == Waypoints.Count - 1);
+    }
+
+    public void AdvanceToNext()
+    {
+        if (Looped)
+        {
+            next = (next + 1) % Waypoints.Count;
+        }
+        else
+        {
+            if (next != Waypoints.Count - 1)
+            {
+                next = next + 1;
+            }
+        }
+    }
+}
+
