36dc7033cf870eaad6b68ee7de70c9b929c36e18
outerheaven182 <mark.corrigan@yahoo.co.uk>
2014-05-19 20:02:51 +0100

    5th Commit - Dodgy Health Scripts for Enemies

---
+++ LabExam/GamesAIBasics-master/Assets/Health.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour
+{
+	int	health = 10;
+
+		// Use this for initialization
+		void Start ()
+		{
+			
+		}
+	
+		// Update is called once per frame
+		void OnTriggerEnter(Collider theEnterer )
+		{
+			if( theEnterer.tag == "Lazer")
+			{
+				if(health >=1)
+				{
+					health = health - 1;
+
+						if(health == 0)
+						{
+							Destroy(this.gameObject);
+						}
+				}
+			}
+			
+		}
+}
+
